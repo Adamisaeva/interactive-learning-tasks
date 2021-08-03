445b70a2eea643ca8365622d5b43af1f3ba95950
GITHUBUSERNAME <EMAILADDRESS>
2021-08-03 01:27:35 +0000

    fixed code

--- rout53/rout53.tf
+++ rout53/rout53.tf
@@ -1,7 +1,7 @@
-# resource "aws_route53_record" "www" {
-#   zone_id = aws_route53_zone.id
-#   name    = "blog.missadam.com"
-#   type    = "A"
-#   ttl     = "300"
-#   records = "127.0.0.1"
-# }
+resource "aws_route53_record" "www" {
+  zone_id = "Z07310871ZNVMWSIHPDCZ"
+  name    = "blog.missadam.com"
+  type    = "A"
+  ttl     = "300"
+  records = ["127.0.0.1"]
+}
